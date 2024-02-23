eedd72ca6d2a1e3eacbcb33f4e21dab2f6bcccb9
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-02-23 13:48:56 +0530

    angularCollage Setup mswd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes  , RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  
+        { path: '', component: HomeComponent },
+        { path: 'about', component: AboutComponent },
+   
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
